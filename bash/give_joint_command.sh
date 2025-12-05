cdf7720a1e88111e5d6b99ff126f80a763494a41
Jialei Li <lijialei829@gmail.com>
2025-12-06 00:37:22 +0100

    add so100 usd; update give_joint_command.sh accordingly

--- bash/give_joint_command.sh
+++ bash/give_joint_command.sh
@@ -9,9 +9,9 @@ header:
   stamp: {sec: 0, nanosec: 0}
   frame_id: ''
 name:
-  ['panda_joint1','panda_joint2','panda_joint3','panda_joint4','panda_joint5','panda_joint6','panda_joint7','panda_finger_joint1','panda_finger_joint2']
+  ['shoulder_pan','shoulder_lift','elbow_flex','wrist_flex','wrist_roll','gripper']
 position:
-  [0.0, -1.16, 0.0, -2.3, 0.0, 1.6, 1.1, 0.4, 0.4]
+  [21.0, 40.0, -40.0, -65.0, 20.0, 20.0]
 " -1
 
 
