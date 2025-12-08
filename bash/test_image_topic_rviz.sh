8ef44cf62fbb96fe31520e82c993407775dab645
Jialei Li <lijialei829@gmail.com>
2025-12-08 20:26:23 +0100

    update .sh

--- bash/test_image_topic_rviz.sh
+++ bash/test_image_topic_rviz.sh
@@ -4,6 +4,6 @@
 # Start Rviz with stored layout config
 # =====================================
 
-rviz2 -d ~/isaacsim_vla_ws/config/vla_franka_2cam.rviz
+rviz2 -d ~/isaacsim_vla_ws/config/vla_so100_2cam.rviz
 
 
