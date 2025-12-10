e10258c67babe805cae0a7c0eedd60174ec34974
Jialei Li <lijialei829@gmail.com>
2025-12-10 23:12:15 +0100

    record some action commands from SmolVLA

--- bash/give_joint_command.sh
+++ bash/give_joint_command.sh
@@ -7,6 +7,17 @@
 # default home position:
 # [0.0009, 0.0258, 0.0, 0.0, 0.0, -0.0065]
 
+# 1st action
+# [-0.726, 0.251, 0.321, 0.364, 1.441, 2.776]
+
+# 2nd action
+# [-0.01296126,  1.7586157 , -1.5670338 , -0.64176947,  0.10169099, 0.89535266]
+
+# 3rd action
+# [ 0.00373928,  1.7602636 , -1.5617038 , -0.6458767 ,  0.09015466, 0.89725024]
+
+# then get stuck at above position
+
 ros2 topic pub /joint_command sensor_msgs/msg/JointState "
 header:
   stamp: {sec: 0, nanosec: 0}
@@ -14,7 +25,7 @@ header:
 name:
   ['shoulder_pan','shoulder_lift','elbow_flex','wrist_flex','wrist_roll','gripper']
 position:
-  [-1.0, 0.0, 0.0, 0.0, 0.0, -0.0065]
+  [-0.726, 0.251, 0.321, 0.364, 1.441, 2.776]
 " -1
 
 
